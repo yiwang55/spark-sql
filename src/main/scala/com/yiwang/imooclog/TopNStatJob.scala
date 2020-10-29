0b3f496f0866d432dceb27328e2141bdd4b584fe
wangyi <wangyi@ghcchina.com.cn>
2020-10-29 09:06:38 +0800

    数据统计  使用dataframe api和 sql两种不同方式进行统计

--- src/main/scala/com/yiwang/imooclog/TopNStatJob.scala
+++ src/main/scala/com/yiwang/imooclog/TopNStatJob.scala
@@ -1,7 +1,46 @@
 package com.yiwang.imooclog
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 object TopNStatJob {
-  val spark = SparkSession.builder().appName("SparkStatCleanJob").master("local[2]").getOrCreate()
+
+  def videoAccessTopNStat(spark: SparkSession, logDF: DataFrame) = {
+    /**
+     * DataFrame API方式统计
+     */
+    //    import spark.implicits._
+//    val result = logDF.filter($"day"==="20170511" && $"cmsType"==="video")
+//      .groupBy("day","cmsId")
+//      .agg(count("cmsId").as("times"))
+//      .orderBy($"times".desc)
+
+    /**
+     * spark sql方式统计
+     */
+    logDF.createOrReplaceTempView("access_logs")
+    val result = spark.sql("select " +
+      "day,cmsId,count(cmsId) as times   " +
+      "from access_logs " +
+      "where day = '20170511' and cmsType='video'  " +
+      "group by day,cmsId " +
+      "order by times desc")
+    result.show(false)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("TopNStatJob")
+      .master("local[2]")
+      .config("spark.sql.sources.partitionColumnTypeInference.enabled","false")
+      .getOrCreate()
+
+    val logDF = spark.read.format("parquet").load("D:\\bigdata\\codefiles\\sparkSQL\\format_cleaned.log")
+
+//    logDF.printSchema()
+//    logDF.show(10, false)
+
+    videoAccessTopNStat(spark, logDF)
+
+    spark.stop()
+  }
 }
